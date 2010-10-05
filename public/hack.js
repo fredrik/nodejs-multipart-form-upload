84bee5b6ed224593e9e9e8bb4f7e345f6b525a35
Fredrik Möllerstrand <fredrik@mollerstrand.se>
2010-10-05 18:45:48 +0100

    add listener for click event

--- public/hack.js
+++ public/hack.js
@@ -1,1 +1,18 @@
 // empties
+$(document).ready(function() {
+  // register event on submit_button#click()
+  $('#submit_button').click(function(e) {
+
+    var f = $("input:file");
+    if (f.val() == '') {
+      $('#status').css({background: 'red'});
+      $('#status').html('INSERT DISK.')
+    } else {
+      $('#status').css({background: 'white'});
+      $('#status').html('uploading: ' + f.val());
+      // TODO: disable submit button.
+      // TODO: submit form.
+      // TODO: start polling localhost:8000/status
+    }
+  })
+});
