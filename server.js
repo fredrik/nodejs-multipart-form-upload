ff2cf4f15a054f8afd9e1d72e5002ded6144afa9
Fredrik Möllerstrand <fredrik@mollerstrand.se>
2010-10-10 19:26:42 +0100

    keep track of progress.

--- server.js
+++ server.js
@@ -7,16 +7,31 @@ var sys        = require('sys'),
 var PUBLIC = path.join(path.dirname(__filename), 'public');
 
 http.createServer(function(req, res) {
+  var progresses = {}
 
   // parse a file upload using formidable
   if (req.url == '/upload' && req.method.toLowerCase() == 'post') {
     var form = new formidable.IncomingForm();
-      form.parse(req, function(fields, files) {
-        res.writeHead(200, {'content-type': 'text/html'});
-        res.end('<textarea>heh</textarea>')
-        // res.write('received upload:\n\n');
-        // res.end(sys.inspect({fields: fields, files: files}));
-      });
+    form.uploadDir = './data';
+    form.keepExtensions = true;
+
+    // keep track of progress.
+    form.addListener('progress', function(recvd, expected) {
+      progress = (recvd / expected * 100).toFixed(2);
+      progresses['unique_key'] = progress
+    });
+
+    // done.
+    form.parse(req, function(error, fields, files) {
+      var path     = files['file']['path'],
+          filename = files['file']['filename'],
+          mime     = files['file']['mime']
+      res.writeHead(200, {'content-type': 'text/html'});
+      res.write('<textarea>');
+      res.write(filename + ' landed at ' + path + '\n');
+      res.write('</textarea>')
+      res.end()
+    });
     return;
   }
 
