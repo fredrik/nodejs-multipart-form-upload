c69557c12fd02a036e61c3ef06d6acfe6869c424
cesine <cesine@tower>
2011-09-16 12:25:38 -0400

    tested praat connection and output is working

--- audio2text.sh
+++ audio2text.sh
@@ -5,25 +5,26 @@ if [ -z "$1" ]; then
 fi
 echo "======================================================="
 
-echo Moving to data folder
+echo ==Moving to data folder
 cd ../nodejs-pocketsphinxdata
 
-echo Creating files _client, _server and .srt
+cp ../nodejs-pocketsphinxtemp/$1_client.srt ../nodejs-pocketsphinxtemp/$1_server.srt
+echo ==Creating files _client, _server and .srt
 cp ../nodejs-pocketsphinxtemp/$1_client.srt $1.srt
 
-echo Branching to user branch
+echo ==Branching to user branch
 git checkout User
 git add *.srt
 git commit -m "added file $1 from user"
 
-echo Branching to machinetranscription branch
+echo ==Branching to machinetranscription branch
 git checkout MachineTranscription
 git merge User
 
-echo Converting mp3/amr $1.amr to pcm/wav $1.wav
+echo ==Converting mp3/amr $1.amr to pcm/wav $1.wav
 ffmpeg -y -i ../nodejs-pocketsphinxtemp/$1.amr ../nodejs-pocketsphinxtemp/$1.wav
 
-echo Running pocketsphinx
+echo ==Running pocketsphinx
 echo "0:00:00.020,0:00:00.020\nResults of the machine transcription will appear below when ready.\n\n" >> $1.srt
 java -jar ../nodejs-pocketsphinx/sphinx4files/transcriber/bin/Transcriber.jar ../nodejs-pocketsphinxtemp/$1.wav >> $1.srt 
 cp $1.srt ../nodejs-pocketsphinxtemp/$1_server.srt
@@ -31,18 +32,18 @@ cp $1.srt ../nodejs-pocketsphinxtemp/$1_server.srt
 #./hello_ps goforward.raw | grep Recognized >> ../../nodejs-pocketsphinxdata/$1
 #cd ../../nodejs-pocketsphinxdata
 
-echo "Committing new transcripion"
+echo "==Committing new transcripion"
 git add *.srt
 git commit -m "ran pocketsphinx on $1"
 
-echo "Processing prosody with Praat"
-praat ../nodejs-pocketsphinx/praatfiles/praat-script-syllable-nuclei-v2file.praat -25 2 0.3 yes ../nodejs-pocketsphinxtemp/$1.wav >>praatresults.csv
+echo "==Processing prosody with Praat"
+praat ../nodejs-pocketsphinx/praatfiles/praat-script-syllable-nuclei-v2file.praat -25 2 0.3 yes /home/gina/aublog/nodejs-pocketsphinxworkspace/nodejs-pocketsphinxtemp $1.wav | tee -a praatresults.csv
 
-echo "Commiting new acoustic results"
+echo "==Commiting new acoustic results"
 git add praatresults.csv
 git commit -m "ran praat on $1"
 
-echo "Server transcription is ready."
+echo "==Server transcription is ready."
 #git checkout master #leave it in the MachineTranascription branch so that the node will copy the right version of the file into the server's response.
 
 cd ../nodejs-pocketsphinx
